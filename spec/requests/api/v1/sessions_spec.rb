d091b2bb26b71d9c27944087e16fe88586069b9a
Andréa Alencar <andreaflether@gmail.com>
2020-12-19 18:00:35 -0300

    Adding destroy action to the sessions controller with tests

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -33,4 +33,15 @@ RSpec.describe 'Sessions API', type: :request do
       it { expect(json_body).to have_key(:errors) }
     end
   end
+
+  describe 'DELETE /sessions/:id' do
+    let(:auth_token) { user.auth_token }
+
+    before do 
+      delete "/sessions/#{auth_token}", params: {}, headers: headers
+    end
+
+    it { expect(response).to have_http_status(204) }
+    it { expect(User.find_by(auth_token: auth_token)).to be_nil }
+  end
 end
