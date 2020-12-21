c0bd5a8fc2c8170b6a4ed1021234dd2edcad2d1e
Andréa Alencar <andreaflether@gmail.com>
2020-12-21 00:54:43 -0300

    Adding API::V2

---
+++ spec/requests/api/v2/tasks_spec.rb
@@ -0,0 +1,85 @@
+require 'rails_helper'
+
+RSpec.describe 'Tasks API', type: :request do 
+  before { host! 'api.task-manager.test' }
+
+  let!(:user) { create(:user) }
+  let(:headers) do
+    {
+      'Accept': 'application/vnd.taskmanager.v2',
+      'Content-Type': Mime[:json].to_s,
+      'Authorization': user.auth_token
+    }
+  end
+
+  describe 'GET /tasks' do 
+    before do
+      create_list(:task, 5, user_id: user.id)
+      get '/tasks', params: {}, headers: headers
+    end
+
+    it { expect(response).to have_http_status(200) }
+    it { expect(json_body[:tasks].count).to eq(5) }
+  end
+
+  describe 'GET /tasks/:id' do 
+    let(:task) { create(:task, user_id: user.id) }
+
+    before { get "/tasks/#{task.id}", params: {}, headers: headers }
+    
+    it { expect(response).to have_http_status(200) }
+    it { expect(json_body[:title]).to eq(task.title)}
+  end
+
+  describe 'POST /tasks' do 
+    before { post '/tasks', params: { task: task_params }.to_json, headers: headers } 
+  
+    context 'when the params are valid' do
+      let(:task_params) { attributes_for(:task) }
+
+      it { expect(response).to have_http_status(201) }  
+      it { expect(Task.find_by(title: task_params[:title])).not_to be_nil }
+      it { expect(json_body[:title]).to eq(task_params[:title]) }
+      it { expect(json_body[:user_id]).to eq(user.id) }
+    end
+
+    context 'when the params are invalid' do
+      let(:task_params) { attributes_for(:task, title: '') }
+
+      it { expect(response).to have_http_status(422) }
+      it { expect(Task.find_by(title: task_params[:title])).to be_nil }
+      it { expect(json_body[:errors]).to have_key(:title) }
+    end
+  end
+
+  describe 'PUT /tasks/:id' do 
+    let!(:task) { create(:task, user_id: user.id) }
+    
+    before { put "/tasks/#{task.id}", params: { task: task_params }.to_json, headers: headers }
+
+    context 'when the params are valid' do 
+      let(:task_params) { { title: 'New task title' } }
+
+      it { expect(response).to have_http_status(200) }
+      it { expect(json_body[:title]).to eq(task_params[:title])}
+      it { expect(Task.find_by(title: task_params[:title])).not_to be_nil }
+    end
+
+    context 'when the params are invalid' do 
+      let(:task_params) { { title: '' } }
+
+      it { expect(response).to have_http_status(422) }
+      it { expect(json_body[:errors]).to have_key(:title) }
+      it { expect(Task.find_by(title: task_params[:title])).to be_nil }
+    end
+  end
+
+  describe 'DELETE /tasks/:id' do 
+    let!(:task) { create(:task, user_id: user.id) }
+
+    before { delete "/tasks/#{task.id}", params: {}, headers: headers }
+
+    it { expect(response).to have_http_status(204) }
+    it { expect { Task.find(task.id) }.to raise_error(ActiveRecord::RecordNotFound) }
+  end
+end
---
+++ spec/requests/api/v2/users_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do 
+  let!(:user) { create(:user) }
+  let(:user_id) { user.id }
+  let(:headers) do
+    {
+      'Accept': 'application/vnd.taskmanager.v2',
+      'Content-Type': Mime[:json].to_s,
+      'Authorization': user.auth_token
+    }
+  end
+
+  before { host! 'api.task-manager.test' }
+
+  describe 'GET /users/:id' do 
+    before do 
+      get "/users/#{user_id}", params: {}, headers: headers
+    end 
+
+    context 'when the user exists' do 
+      it { expect(json_body[:id]).to eq(user_id) }
+      it { expect(response).to have_http_status(200) }
+    end
+
+    context 'when the user does not exist' do 
+      let(:user_id) { 100000 }
+
+      it { expect(response).to have_http_status(404) }
+    end
+  end
+
+  describe 'POST /users' do 
+    before do 
+      post '/users', params: { user: user_params }.to_json, headers: headers
+    end
+    context 'when the request params are valid' do 
+      let(:user_params) { attributes_for(:user) }
+
+      it { expect(response).to have_http_status(201) }  
+      it { expect(json_body[:email]).to eq(user_params[:email]) }
+    end
+
+    context 'when the request params are invalid' do 
+      let(:user_params) { attributes_for(:user, email: 'invalid-@') }
+
+      it { expect(response).to have_http_status(422) }
+      it { expect(json_body).to have_key(:errors) }
+    end
+  end
+
+  describe 'PUT /users/:id' do 
+    before do 
+      put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
+    end
+    
+    context 'when the request params are valid' do 
+      let(:user_params) { { email: 'new@taskmanager.com' } }
+      it { expect(response).to have_http_status(200) } 
+      it { expect(json_body[:email]).to eq(user_params[:email]) }
+    end
+
+    context 'when the request params are invalid' do 
+      let(:user_params) { attributes_for(:user, email: 'invalid-@') }
+
+      it { expect(response).to have_http_status(422) }
+      it { expect(json_body).to have_key(:errors) }
+    end
+  end
+
+  describe 'DELETE /users/:id' do 
+    before do 
+      delete "/users/#{user_id}", params: { }, headers: headers
+    end
+
+    it { expect(response).to have_http_status(204) }
+    it { expect(User.find_by(id: user.id)).to be_nil }     
+  end
+end
