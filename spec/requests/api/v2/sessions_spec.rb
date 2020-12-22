aaa3eef6b309d73049400d960383d8484e241565
Andréa Alencar <andreaflether@gmail.com>
2020-12-22 14:16:36 -0300

    Updating the sessions API

--- spec/requests/api/v2/sessions_spec.rb
+++ spec/requests/api/v2/sessions_spec.rb
@@ -2,28 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'Sessions API', type: :request do 
   before { host! 'api.task-manager.test' }
-  let(:user) { create(:user) }
-  let(:headers) do 
+
+  let!(:user) { create(:user) }
+  let(:auth_data) { user.create_new_auth_token }
+  let(:headers) do
     {
       'Accept': 'application/vnd.taskmanager.v2',
-      'Content-Type': Mime[:json].to_s
+      'Content-Type': Mime[:json].to_s,
+      'access-token': auth_data['access-token'],
+      'uid': auth_data['uid'],
+      'client': auth_data['client']
     }
   end
 
-  describe 'POST /sessions' do 
-    before do 
-      post '/sessions', params: { session: credentials }.to_json, headers: headers
-    end
+  describe 'POST /auth/sign_in' do 
+    before { post '/auth/sign_in', params: credentials.to_json, headers: headers }
 
     context 'when the credentials are correct' do
       let(:credentials) { { email: user.email, password: '123456' } }
 
       it { expect(response).to have_http_status(200) }
       
-      it 'returns the JSON data for the user with auth token' do 
-        user.reload 
-        expect(json_body[:data][:attributes][:'auth-token']).to eq(user.auth_token)
-      end
+      it { expect(response.headers).to have_key('access-token') }
+      it { expect(response.headers).to have_key('uid') }
+      it { expect(response.headers).to have_key('client') }
     end
 
     context 'when the credentials are incorrect' do
@@ -37,9 +39,7 @@ RSpec.describe 'Sessions API', type: :request do
   describe 'DELETE /sessions/:id' do
     let(:auth_token) { user.auth_token }
 
-    before do 
-      delete "/sessions/#{auth_token}", params: {}, headers: headers
-    end
+    before { delete "/sessions/#{auth_token}", params: {}, headers: headers }
 
     it { expect(response).to have_http_status(204) }
     it { expect(User.find_by(auth_token: auth_token)).to be_nil }
