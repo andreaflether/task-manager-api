7658cf975a848fb3da70ef1ef2ca4c29fa1ee3b3
Andréa Alencar <andreaflether@gmail.com>
2020-12-22 19:15:51 -0300

    Allowing to send name as attribute for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter.sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter.sanitizer.permit(:account_update, keys: [:name])
+  end
 end
